603d30e626e119b9a7ec5ca4aaf8d045f6d5cba2
bareca90 <bareca90@gmail.com>
2024-02-08 15:42:46 -0500

    Se Procede a realizar el primer commit de este repo , que ha sido movido desde la maquina local hasta el servidor de git

---
+++ configuraciones/configuracion_base.js
@@ -0,0 +1,14 @@
+var config = {
+    user: 'sa',
+    password: '84+-blaster32',
+    server: '10.100.120.8',
+    database: 'pSipe4',
+    driver: 'msnodesqlv8',
+    options: {
+        enableArithAbort: true,
+        trustedConnection: true
+    }
+
+};
+module.exports = config;
+
