4a960cb2af40576565824fe1ce28281591e88350
bareca90 <bareca90@gmail.com>
2024-02-08 15:57:16 -0500

    Cambio de servidor y bd

--- configuraciones/configuracion_base.js
+++ configuraciones/configuracion_base.js
@@ -1,8 +1,8 @@
 var config = {
     user: 'sa',
     password: '84+-blaster32',
-    server: '10.100.120.8',
-    database: 'pSipe4',
+    server: '10.100.120.7',
+    database: 'SipeDes',
     driver: 'msnodesqlv8',
     options: {
         enableArithAbort: true,
